ede4aed8ca88129062cd178edde70127d1d913b0
azord <azord@DESKTOP-N59VBOJ>
2019-02-12 10:51:53 +0100

    versión refactorizada

--- geometria/src/Circulo_AMS.java
+++ geometria/src/Circulo_AMS.java
@@ -1,10 +1,10 @@
 //versión inicial
-public class Circulo extends FiguraGeometrica {
+public class Circulo_AMS extends FiguraGeometrica_AMS {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_AMS(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_AMS.java
+++ geometria/src/FiguraGeometrica_AMS.java
@@ -1,9 +1,9 @@
 //versión inicial
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_AMS {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_AMS(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
